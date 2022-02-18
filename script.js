e5d5e0151e4bb18b3f7983b1e84a0953ff128f25
Vinay Parmar <vinayparmarjain@hotmail.com>
2022-02-18 16:36:38 +0530

    Add : Floating Menu

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-window.addEventListener("load", function () {
-  setTimeout(function autoClose(event) {
-    document.getElementById("modal").style.display = "none";
-  }, 10000);
-});
-
-document.getElementById("close").addEventListener("click", function () {
-  document.getElementById("modal").style.display = "none";
-});
-
 const greeting = document.getElementById("greeting");
+const hex = document.getElementById("hex");
+
+// Greeting Message
 const hour = new Date().getHours();
 const welcomeTypes = [
-  "Good morning",
-  "Good afternoon",
-  "Good evening",
-  "Good night",
+  "Good Morning",
+  "Good Afternoon",
+  "Good Evening",
+  "Good Night",
 ];
 let welcomeText = "";
 
@@ -23,4 +16,24 @@ else if (hour < 18) welcomeText = welcomeTypes[1];
 else if (hour < 22) welcomeText = welcomeTypes[2];
 else welcomeText = welcomeTypes[3];
 
-greeting.innerHTML = welcomeText;
+greeting.innerHTML = welcomeText + ", Guest!";
+
+// Modal auto close
+
+window.addEventListener("load", function () {
+  setTimeout(function autoClose(event) {
+    document.getElementById("modal").style.display = "none";
+  }, 10000);
+});
+
+// Modal close button
+
+document.getElementById("close").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "none";
+});
+
+// Floating menu
+
+hex.addEventListener("keyup", function () {
+  document.querySelector(".background-color").style.background = hex.value;
+});
